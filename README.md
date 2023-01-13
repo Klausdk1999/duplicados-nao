97c8054c0ff1b0adf8ac60bb14119e266c0eaab8
Klausdk1999 <klausdk1999@gmail.com>
2023-01-13 09:36:11 -0300

    desafio

---
+++ README.md
@@ -0,0 +1,3 @@
+# Duplicados não
+
+Este desafio envolve o uso de arrays e se trata da criação de uma função que, ao receber dois arrays, deve imprimir os elementos que se encontram tanto em um quanto o outro. Os dois arrays são definidos pelo sistema, e não pelo usuário.
---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        String[] arrayUm = { "banana", "jaca", "dia", "dois", "klaus" };
+        String[] arrayDois = { "pera", "jacaré", "limão", "ave", "klaus" };
+        ArrayList<String> saida = buscarSemelhantes(arrayUm, arrayDois);
+
+        System.out.println("Os elementos repetidos são");
+        for(int i = 0; i < saida.size(); i++) {
+            System.out.println(saida.get(i));
+        }
+    }
+
+    private static ArrayList<String> buscarSemelhantes(String[] arrayUm, String[] arrayDois) {
+        ArrayList<String> saida = new ArrayList<>();
+
+        for (int i = 0; i < arrayUm.length; i++) {
+            for (int j = 0; j < arrayDois.length; j++) {
+                if(arrayUm[i].equals(arrayDois[j])) {
+                    saida.add(arrayUm[i]);
+                }
+            }
+        }
+
+        return saida;
+    }
+}
